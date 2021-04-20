67a373ed5b1ff84191985bbb5bdb69d90b3da817
Arda Kalaycı <klayc.arda@gmail.com>
2021-04-21 00:00:56 +0300

    hata

--- lib/data/api/category_api.dart
+++ lib/data/api/category_api.dart
@@ -1,10 +1,12 @@
+import 'dart:convert';
+
 import 'package:http/http.dart' as http;
 class CategoryApi{
 
    static Future getCategories() async{
        
-        var response=await http.get(Uri.http("10.0.2.2:3000","/categories"));
+        var response=await http.get(Uri.parse("https://run.mocky.io/v3/16ded805-2d03-45b8-968b-d77eaf5a3d68"));
         print(response.body);
-      return response;
+      return jsonDecode(response.body);
   }
 }
